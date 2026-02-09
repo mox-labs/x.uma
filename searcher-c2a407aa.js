e526f1cc1ea70e7b7ed915b4b36791f9618b9c7c
mox-nexus <mox-nexus@users.noreply.github.com>
2026-02-09 01:58:54 +0000

    deploy: bdd40cc9941825da87709660382785230d9fe904

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1e1393d1.js',
+                path_to_root + 'searchindex-6b1472f2.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
