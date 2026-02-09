5aed7dc5408cf26e92d031e0ff6da0a7c58d9604
mox-nexus <mox-nexus@users.noreply.github.com>
2026-02-09 00:17:32 +0000

    Deploy preview for PR 9 🛫

--- pr-preview/pr-9/searcher-c2a407aa.js
+++ pr-preview/pr-9/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a2ea8057.js',
+                path_to_root + 'searchindex-6b1472f2.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
